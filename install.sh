56db882317cd0f5f19127a18200feb12459fade9
Dan Miller <dsmiller95@gmail.com>
2021-08-03 12:58:03 -0500

    got it running

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+chmod --recursive 777 ./cgi-bin/mnemo_ops
 cp --preserve=mode -R ./cgi-bin/* /usr/local/apache2/cgi-bin
-cp -r ./htdocs/* /usr/local/apache2/htdocs
+cp -R ./htdocs/* /usr/local/apache2/htdocs
 cp ./httpd.conf /usr/local/apache2/conf/httpd.conf
 
